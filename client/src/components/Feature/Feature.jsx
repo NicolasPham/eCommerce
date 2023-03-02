4dc34fc9361e2cc537da4ee8851d68db930179d1
Nicolas Pham <phd.ngoc163@gmail.com>
2023-03-02 15:57:12 +0000

    updated

--- client/src/components/Feature/Feature.jsx
+++ client/src/components/Feature/Feature.jsx
@@ -8,7 +8,7 @@ const Feature = ({ type }) => {
 
     return (
         <section className='feature'>
-            <div className="title">
+            <div className="title flexRow">
                 <h1>{type} products</h1>
                 <p>Here you will find the latest and greatest fashion items from our store. Whether you're looking for a new dress, a stylish pair of shoes, or the perfect accessory to complete your look, we have something for everyone. Our featured products are carefully selected to ensure that you always look your best. Shop now and find the perfect item for any occasion!</p>
             </div>
