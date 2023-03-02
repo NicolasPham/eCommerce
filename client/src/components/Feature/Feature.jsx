3292a4a7c9bf7c58759b253c7f0c498644d8f26d
Nicolas Pham <phd.ngoc163@gmail.com>
2023-03-02 15:54:06 +0000

    updated

---
+++ client/src/components/Feature/Feature.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from 'react'
+import "./feature.scss"
+
+import { data } from '../../constant/featuredData.js'
+import Card from '../Card/Card.jsx'
+
+const Feature = ({ type }) => {
+
+    return (
+        <section className='feature'>
+            <div className="title">
+                <h1>{type} products</h1>
+                <p>Here you will find the latest and greatest fashion items from our store. Whether you're looking for a new dress, a stylish pair of shoes, or the perfect accessory to complete your look, we have something for everyone. Our featured products are carefully selected to ensure that you always look your best. Shop now and find the perfect item for any occasion!</p>
+            </div>
+
+            <div className="cards">
+                {data.map(item => (
+                    <Card item={item} key={item.id} />
+                ))}
+            </div>
+        </section>
+    )
+}
+
+export default Feature
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Slider from '../../components/Slider/Slider'
+import Feature from '../../components/Feature/Feature'
+
 
 const Home = () => {
     return (
         <main className='home'>
             <Slider />
+            <Feature type="Featured" />
+            <Feature type="Trending" />
         </main>
     )
 }
