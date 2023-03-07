5939134d3d6a1e7e577b4230db22a72fac1284d4
Nicolas Pham <phd.ngoc163@gmail.com>
2023-03-07 12:32:16 +0000

    done categories

--- client/src/components/Categories/Categories.jsx
+++ client/src/components/Categories/Categories.jsx
@@ -1,31 +1,45 @@
-import React from 'react'
+import React from "react";
 import "./categories.scss";
-import Cat from './Cat'
 
-import {categories} from '../../constant/categories.js'
+import { categories } from "../../constant/categories.js";
 
 const Categories = () => {
-  return (
-    <div className='categories flexRow'>
-        <div className="left flexCol">
-            <Cat item={categories[0]}  />
-            <Cat item={categories[1]}  />
+  return <div className="categories flexRow">
+    <div className="row cat_wrapper">
+      <div className="col">
+        <div className="row">
+          <img src={categories[0].url} alt="" />
+          <button className="title">{categories[0].title}</button>
         </div>
-        <div className="center">
-            <Cat item={categories[2]}  />
+        <div className="row">
+          <img src={categories[1].url} alt="" />
+          <button className="title">{categories[1].title}</button>
         </div>
-        <div className="right flexCol">
-            <div className="top flexRow">
-                <Cat item={categories[3]}  />
-                <Cat item={categories[4]} />
-            </div>
-            <div className="bottom">
-                <Cat item={categories[5]} />
-            </div>
-
+      </div>
+      <div className="col">
+        <div className="row">
+          <img src={categories[2].url} alt="" />
+          <button className="title">{categories[2].title}</button>
         </div>
+      </div>
+      <div className="col-lg">
+        <div className="row">
+          <div className="col">
+            <img src={categories[3].url} alt="" />
+            <button className="title">{categories[3].title}</button>
+          </div>
+          <div className="col">
+            <img src={categories[4].url} alt="" />
+            <button className="title sm">{categories[4].title}</button>
+          </div>
+        </div>
+        <div className="row">
+          <img src={categories[5].url} alt="" />
+          <button className="title">{categories[5].title}</button>
+        </div>
+      </div>
     </div>
-  )
-}
+  </div>;
+};
 
-export default Categories
+export default Categories;
